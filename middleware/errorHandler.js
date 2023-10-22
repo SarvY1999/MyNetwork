d411ab548508c81e15525eebe0642f5c07d88e3f
SarvY1999 <sarveshy193@gmail.com>
2023-10-22 16:34:58 +0530

    added error handler middleware

---
+++ middleware/errorHandler.js
@@ -0,0 +1,11 @@
+const CustomError = require('../customError/customError');
+
+const errorHandler = (err, req, res, next) => {
+    if (err instanceof CustomError) {
+        return res.status(err.errorCode).json({ msg: err.message });
+    }
+
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Something went wrong, Please try again" });
+}
+
+module.exports = errorHandler;
