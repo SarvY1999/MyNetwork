71cf1976e3a761d49f91b1f941e1aa2ea4f106de
SarvY1999 <sarveshy193@gmail.com>
2023-10-22 16:36:11 +0530

    adds app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+// Package Imports
+const express = require('express');
+require('dotenv').config();
+require('express-async-errors')
+const app = express();
+
+// function imports
+const connectDb = require('./Db/connectDb');
+const userRoutes = require('./routes/userRoutes');
+const errorHandler = require('./middleware/errorHandler');
+
+
+app.use(express.json());
+app.use('/api/v1/users', userRoutes);
+app.use(errorHandler)
+
+app.get('/api/v1/', (req, res) => {
+    res.send('MyNetwork is online');
+})
+
+const start = async () => {
+    const port = process.env.PORT || 4000;
+    try {
+        await connectDb(process.env.URI);
+        app.listen(port, () => {
+            console.log(`Server is listening on port: ${port}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
