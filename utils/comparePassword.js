bae744adab5e95dba23578af715d8195a044caf7
unknown <sarveshy193@gmail.com>
2023-10-26 19:12:21 +0530

    removed unnecessary logs

--- utils/comparePassword.js
+++ utils/comparePassword.js
@@ -3,8 +3,6 @@ const CustomError = require('../customError/customError');
 const { StatusCodes } = require('http-status-codes');
 
 const comparePassword = async (password, hashedPassword) => {
-    console.log("passoword provided by user: ", password);
-    console.log("passoword provided by db: ", hashedPassword);
     const isMatch = await bcrypt.compare(password, hashedPassword);
     if (!isMatch) {
         throw new CustomError('Invalid Credentials', StatusCodes.UNAUTHORIZED);
