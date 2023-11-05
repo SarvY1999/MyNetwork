3c69c1399dc0f6e681f3e77813f666869b9929a5
unknown <sarveshy193@gmail.com>
2023-11-05 18:11:13 +0530

    adds code to hadle duplicate error on like

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -6,6 +6,10 @@ const errorHandler = (err, req, res, next) => {
         return res.status(err.errorCode).json({ msg: err.message });
     }
 
+    if(err.code == 11000){
+        return res.status(StatusCodes.OK).json({ msg: 'You have already liked the post' })
+    }
+
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Something went wrong, Please try again" });
 }
 
