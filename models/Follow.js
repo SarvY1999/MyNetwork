0c910313b061b354b84486e2729cd38c73fc402f
unknown <sarveshy193@gmail.com>
2023-11-11 20:57:47 +0530

    removes comment

--- models/Follow.js
+++ models/Follow.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-// 1. Follow : Follower = my Id , following = user i want to follow
 const FollowSchema = new mongoose.Schema({
     following : {
         type: mongoose.Schema.ObjectId,
