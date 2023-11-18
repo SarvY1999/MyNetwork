191afbb4a1217c4d4d480fdebba155c16393024c
unknown <sarveshy193@gmail.com>
2023-11-18 16:35:14 +0530

    adds code in hasedpassword to check is password is getting modified or not

--- models/User.js
+++ models/User.js
@@ -46,6 +46,7 @@ Userschema.virtual('following', {
 })
 
 Userschema.pre('save', async function () {
+    if(!this.isModified('password')) return;
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 })
