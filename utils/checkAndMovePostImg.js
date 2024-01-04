72ca6198f090ed4a38562a72abc37f9f132db5cc
unknown <sarveshy193@gmail.com>
2024-01-04 19:41:41 +0530

    adds code to make image unqiue even if user uploads same image twice

--- utils/checkAndMovePostImg.js
+++ utils/checkAndMovePostImg.js
@@ -1,9 +1,7 @@
 const { StatusCodes } = require("http-status-codes")
 const CustomError = require("../customError/customError")
 const path = require('path')
-const crypto = require('crypto');
 
-// work on generating a random string and attach it to the image so that image will be unique evry time
 const checkAndMovePostImage = async (img) => {
     if (!img.mimetype.startsWith('image')) {
         throw new CustomError('Please select an image', StatusCodes.BAD_REQUEST);
@@ -14,8 +12,7 @@ const checkAndMovePostImage = async (img) => {
         throw new CustomError('Image is too big, please select an image smaller than 5 MB ', StatusCodes.BAD_REQUEST);
     }
 
-    const hash = crypto.createHash('md5').update(img.data).digest('hex');
-    const imgPath = path.join(__dirname, `../public/postImg/` + `${hash}${img.name}`);
+    const imgPath = path.join(__dirname, `../public/postImg/` + `${Date.now()}${img.name}`);
     await img.mv(imgPath)
     return imgPath;
 }
